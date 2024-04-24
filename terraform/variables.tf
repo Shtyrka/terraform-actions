760c28abb55f656e8a0d253dd2a51ed5d4b0af94
Tony Sh <128180505+Anatolich911@users.noreply.github.com>
2024-04-24 13:16:57 -0400

    test

---
+++ terraform/variables.tf
@@ -0,0 +1,51 @@
+# variables.tf
+variable "name" {
+  description = "The name of the security group"
+  type        = string
+}
+
+variable "description" {
+  description = "The description of the security group"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The VPC ID where the security group should be created"
+  type        = string
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rules"
+  type        = list(object({
+    description      = string
+    from_port        = number
+    to_port          = number
+    protocol         = string
+    cidr_blocks      = list(string)
+    ipv6_cidr_blocks = list(string)
+    security_groups  = list(string)
+    prefix_list_ids  = list(string)
+  }))
+  default     = []
+}
+
+variable "egress_rules" {
+  description = "List of egress rules"
+  type        = list(object({
+    description      = string
+    from_port        = number
+    to_port          = number
+    protocol         = string
+    cidr_blocks      = list(string)
+    ipv6_cidr_blocks = list(string)
+    security_groups  = list(string)
+    prefix_list_ids  = list(string)
+  }))
+  default     = []
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource"
+  type        = map(string)
+  default     = {}
+}
